8d92bddce7cd9f9dc033663b58fbb3a4e284b2d1
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2021-10-20 16:21:59 +0400

    Adding conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,20 +11,26 @@ function Expenses(props){
         setFilteredYear(year);
     }
 
+    const filteredExpenses = props.expenses.filter(x => x.date.getFullYear().toString() === filteredYear);
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map(expense => (
+            <ExpenseItem 
+                key = {expense.id}
+                title = {expense.title}
+                amount = {expense.amount}
+                date = {expense.date}
+            />
+        ));
+    }
+
     return(
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onYearChange={onYearChangeHandler} />
-                {
-                    props.expenses.filter(x => x.date.getFullYear().toString() === filteredYear).map(expense => (
-                        <ExpenseItem 
-                            key = {expense.id}
-                            title = {expense.title}
-                            amount = {expense.amount}
-                            date = {expense.date}
-                        />
-                    ))
-                }
+                {expensesContent}
             </Card>
         </div>
     ); 
